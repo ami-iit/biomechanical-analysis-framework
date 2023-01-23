a655e868007d550090d363faadde3bd43d5ce7ee
Riccardo <riccardo.grieco@hotmail.com>
2023-01-23 15:43:37 +0100

    Add CI

--- cmake/BiomechanicalAnalysisFrameworkDependencies.cmake
+++ cmake/BiomechanicalAnalysisFrameworkDependencies.cmake
@@ -5,12 +5,6 @@
 ########################## Mandatory dependencies ###############################
 # Find all packages
 
-find_package(iDynTree 1.1.0 REQUIRED) 
-
-find_package(Eigen3 3.2.92 REQUIRED)
-
-find_package(OsqpEigen 0.4.0 REQUIRED)
-
 find_package(BipedalLocomotionFramework 0.11.200 REQUIRED) #TODO version
 
 ########################## Optional dependencies  ##############################
