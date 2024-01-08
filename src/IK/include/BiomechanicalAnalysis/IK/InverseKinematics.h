c767a51707c35d6b88aa340492dd80f484385d50
Davide <davidegorbani@gmail.com>
2024-01-08 14:59:05 +0100

    document code using doxygen format

--- src/IK/include/BiomechanicalAnalysis/IK/InverseKinematics.h
+++ src/IK/include/BiomechanicalAnalysis/IK/InverseKinematics.h
@@ -62,47 +62,73 @@ public:
     HumanIK(){}; // constructor
     ~HumanIK(){}; // destructor
 
-    // initialize all the task and the inverse kinematics solver
+    /**
+     * initialize all the task and the inverse kinematics solver
+    */
     bool initialize(std::weak_ptr<const BipedalLocomotion::ParametersHandler::IParametersHandler> handler,
                 std::shared_ptr<iDynTree::KinDynComputations> kinDyn);
 
-    // set the integration time step
+    /**
+     * set the integration time step
+    */
     bool setDt(const double dt);
 
-    // get the integration time step
+    /**
+     * get the integration time step
+    */
     double getDt() const;
 
-    // set the number of DoFs
+    /**
+     * set the number of DoFs
+    */
     bool setDoFsNumber(const int nrDoFs);
 
-    // get the number of DoFs
+    /**
+     * get the number of DoFs
+    */
     int getDoFsNumber() const;
 
-    // set the initial joint positions
+    /**
+     * set the initial joint positions
+    */
     bool setInitialJointPositions(const Eigen::Ref<const Eigen::VectorXd> qInitial);
 
     bool setLink1OrientationAndAngVel(const manif::SO3d &link1Orientation,
                                       const manif::SO3Tangentd &link1AngularVelocity);
 
-    // compute the next state
+    /**
+     * advance the inverse kinematics solver
+    */
     bool advance();
 
-    // get the joint poistions
+    /**
+     * get the joint positions
+    */
     bool getJointPositions(Eigen::Ref<Eigen::VectorXd> jointPositions) const;
 
-    // get the joint velocities
+    /**
+     * get the joint velocities
+    */
     bool getJointVelocities(Eigen::Ref<Eigen::VectorXd> jointVelocities) const;
 
-    // get the base position
+    /**
+     * get the base position
+    */
     bool getBasePosition(Eigen::Ref<Eigen::Vector3d> basePosition) const;
 
-    // get the base velocity
+    /**
+     * get the base linear velocity
+    */
     bool getBaseLinearVelocity(Eigen::Ref<Eigen::Vector3d> baseVelocity) const;
 
-    // get the base orientation
+    /**
+     * get the base orientation
+    */
     bool getBaseOrientation(Eigen::Ref<Eigen::Matrix3d> baseOrientation) const;
 
-    // get the base angular velocity
+    /**
+     * get the base angular velocity
+    */
     bool getBaseAngularVelocity(Eigen::Ref<Eigen::Vector3d> baseAngularVelocity) const;
 };
 
