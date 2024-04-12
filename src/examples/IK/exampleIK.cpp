d9953e3daf2d920223996140e07470efd129ffc8
gianlucamilani <gianluca.milani@iit.it>
2024-04-12 10:08:15 +0200

    fixed paths

--- src/examples/IK/exampleIK.cpp
+++ src/examples/IK/exampleIK.cpp
@@ -302,11 +302,11 @@ int main()
     }
 
     // Load data from Matlab files
-    matioCpp::File file("C:/Users/gimilani/Desktop/BAFinput/matlab1.mat"); // Create a file object to read data from MATLAB file "matlab1.mat"
-    matioCpp::Struct ifeel_data = file.read("ifeel_data").asStruct(); // Read "ifeel_data" structure from the MATLAB file and convert it to a structure
-    matioCpp::Struct node12 = ifeel_data("iFeelSuit_vLink_Node_12").asStruct(); // Access structure "iFeelSuit_vLink_Node_12" within "ifeel_data" structure and convert it to a structure
-    matioCpp::File file2("C:/Users/gimilani/Desktop/BAFinput/human_data1.mat"); // Create another file object to read data from MATLAB file "human_data.mat"
-    matioCpp::Struct human_data = file2.read("human_data").asStruct(); // Read "human_data" structure from the MATLAB file and convert it to a structure
+    matioCpp::File file("/path/to/matlab1.mat");
+    matioCpp::Struct ifeel_data = file.read("ifeel_data").asStruct();
+    matioCpp::Struct node12 = ifeel_data("iFeelSuit_vLink_Node_12").asStruct();
+    matioCpp::File file2("/path/to/human_data.mat");    matioCpp::Struct human_data = file2.read("human_data").asStruct(); // Read "human_data" structure from the MATLAB file and convert it to a structure
+    matioCpp::Struct human_data = file2.read("human_data").asStruct(); // Read "human_data" structure from the MATLAB file and convert it to a structure    
     matioCpp::Struct human_state = human_data("human_state").asStruct(); // Access structure "human_state" within "human_data" structure and convert it to a structure
     matioCpp::Struct joint_positions = human_state("joint_positions").asStruct(); // Access structure "joint_positions" within "human_state" structure and convert it to a structure
     matioCpp::MultiDimensionalArray<double> jointPos_data = joint_positions("data").asMultiDimensionalArray<double>(); // Access multidimensional array "data" within "joint_positions" structure and convert it to a multidimensional array of type double
