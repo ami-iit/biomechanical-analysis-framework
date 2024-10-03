3caf1e2961d0d101a1cd6aefb968011c7e988ed7
Davide <davidegorbani@gmail.com>
2024-10-03 17:36:11 +0200

    added methods to IK bindings

--- bindings/python/IK/src/InverseKinematics.cpp
+++ bindings/python/IK/src/InverseKinematics.cpp
@@ -11,8 +11,8 @@
 #include <pybind11/stl.h>
 
 #include <BiomechanicalAnalysis/IK/InverseKinematics.h>
-#include <BipedalLocomotion/ParametersHandler/IParametersHandler.h>
 #include <BiomechanicalAnalysis/bindings/type_caster/swig.h>
+#include <BipedalLocomotion/ParametersHandler/IParametersHandler.h>
 
 namespace BiomechanicalAnalysis
 {
@@ -28,15 +28,18 @@ void CreateInverseKinematics(pybind11::module& module)
     using namespace BiomechanicalAnalysis::IK;
     using namespace BipedalLocomotion::ParametersHandler;
 
+    py::class_<nodeData>(module, "nodeData")
+        .def(py::init<>())
+        .def_readwrite("I_R_IMU", &nodeData::I_R_IMU)
+        .def_readwrite("I_omega_IMU", &nodeData::I_omega_IMU);
+
     py::class_<HumanIK>(module, "HumanIK")
         .def(py::init())
         .def(
             "initialize",
-            [](HumanIK& ik,
-                std::shared_ptr<const IParametersHandler> handler, py::object& obj) -> bool {
+            [](HumanIK& ik, std::shared_ptr<const IParametersHandler> handler, py::object& obj) -> bool {
                 std::shared_ptr<iDynTree::KinDynComputations>* cls
-                    = py::detail::swig_wrapped_pointer_to_pybind<
-                        std::shared_ptr<iDynTree::KinDynComputations>>(obj);
+                    = py::detail::swig_wrapped_pointer_to_pybind<std::shared_ptr<iDynTree::KinDynComputations>>(obj);
 
                 if (cls == nullptr)
                 {
@@ -47,8 +50,39 @@ void CreateInverseKinematics(pybind11::module& module)
                 return ik.initialize(handler, *cls);
             },
             py::arg("param_handler"),
-            py::arg("kin_dyn"));
-
+            py::arg("kin_dyn"))
+        .def("setDt", &HumanIK::setDt, py::arg("dt"))
+        .def("getDt", &HumanIK::getDt)
+        .def("getDoFsNumber", &HumanIK::getDoFsNumber)
+        .def("updateOrientationTask",
+             &HumanIK::updateOrientationTask,
+             py::arg("node"),
+             py::arg("I_R_IMU"),
+             py::arg("I_omega_IMU") = manif::SO3d::Tangent::Zero())
+        .def("updateGravityTask", &HumanIK::updateGravityTask, py::arg("node"), py::arg("I_R_IMU"))
+        .def("updateFloorContactTask", &HumanIK::updateFloorContactTask, py::arg("node"), py::arg("verticalForce"))
+        .def("clearCalibrationMatrices", &HumanIK::clearCalibrationMatrices)
+        .def("calibrateWorldYaw", &HumanIK::calibrateWorldYaw, py::arg("nodeStruct"))
+        .def("calibrateAllWithWorld", &HumanIK::calibrateAllWithWorld, py::arg("nodeStruct"), py::arg("frameName"))
+        .def("calibrateWorldYaw", &HumanIK::calibrateWorldYaw, py::arg("nodeStruct"))
+        .def("calibrateAllWithWorld", &HumanIK::calibrateAllWithWorld, py::arg("nodeStruct"), py::arg("frameName"))
+        .def("advance", &HumanIK::advance)
+        .def("updateOrientationGravityTasks", &HumanIK::updateOrientationAndGravityTasks, py::arg("nodeStruct"))
+        .def("updateFloorContactTasks", &HumanIK::updateFloorContactTasks, py::arg("wrenchMap"))
+        .def("updateJointRegularizationTask", &HumanIK::updateJointRegularizationTask)
+        .def("updateJointConstraintsTask", &HumanIK::updateJointConstraintsTask)
+        .def("advance", &HumanIK::advance)
+        .def("getJointPositions",
+             [](HumanIK& ik) {
+                 Eigen::VectorXd jointPositions(ik.getDoFsNumber());
+                 bool ok = ik.getJointPositions(jointPositions);
+                 return std::make_tuple(ok, jointPositions);
+             })
+        .def("getJointVelocities", [](HumanIK& ik) {
+            Eigen::VectorXd jointVelocities(ik.getDoFsNumber());
+            bool ok = ik.getJointVelocities(jointVelocities);
+            return std::make_tuple(ok, jointVelocities);
+        });
 }
 
 } // namespace IK
